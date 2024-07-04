ec7d8bf421ba63b12908fdfa2d3b6663dbc67ff4
aliyunvideo <videosdk@service.aliyun.com>
2024-07-04 20:21:07 +0800

    feat:播放源调整

--- iOS/Source/AUIShortEpisodeDataManager.m
+++ iOS/Source/AUIShortEpisodeDataManager.m
@@ -9,12 +9,17 @@
 #import <AFNetworking/AFNetworking.h>
 #import "AUIFoundation.h"
 
+NSString * const AUI_EPISODE_TEST = @"https://alivc-demo-cms.alicdn.com/versionProduct/resources/player/aui_episode.json";
+NSString * const AUI_EPISODE_TEST_ENCRYPT = @"https://alivc-demo-cms.alicdn.com/versionProduct/resources/player/aui_episode_encrypt.json";
+
+
 @implementation AUIShortEpisodeDataManager
 
 + (void)fetchData:(NSString *)eid completed:(void (^)(AUIShortEpisodeData *, NSError *))completed {
     // TODO: 请求服务端返回短剧数据，此次是仅模拟，有需要请自身实现
+    NSString *url = AUI_EPISODE_TEST;
     AFURLSessionManager *manager = [[AFURLSessionManager alloc] init];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://alivc-demo-cms.alicdn.com/versionProduct/resources/player/aui_episode_encrypt.json"]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
     request.cachePolicy = NSURLRequestReloadIgnoringLocalAndRemoteCacheData;
     NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request uploadProgress:nil downloadProgress:nil completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
         if (error) {
